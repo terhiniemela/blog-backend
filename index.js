9e6d7033c2c5c8148e90d9fae8cf9010104fa2e3
Terhi Niemelä <terhi.niemela@gmail.com>
2024-07-31 22:32:08 +0300

    project hassle

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
 console.log("hello world")
+
+app.use(cors())
+app.use(express.json())
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
